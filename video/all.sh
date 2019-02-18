e2be32617fb9fd97224700d62d52c8a1fc5b79a4
Andrea Mazzoleni <amadvance@gmail.com>
2019-02-18 19:52:17 +0100

    New tracks

--- video/all.sh
+++ video/all.sh
@@ -1,5 +1,5 @@
 python convert.py /gopro /sharphigh /4k /panning forcella_alta_panorama.mp4
-python convert.py /gopro /sharphigh roccolone_parziale.mp4
+#NUKED python convert.py /gopro /sharphigh roccolone_parziale.mp4
 python convert.py /gopro /sharphigh 712sud.mp4
 python convert.py /gopro /sharphigh torre.mp4
 python convert.py /gopro /sharphigh allegrezza.mp4
@@ -8,20 +8,20 @@ python convert.py /gopro /sharphigh pistadh.mp4
 python convert.py /gopro /sharphigh collesogno.mp4
 python convert.py /gopro /sharphigh tesoro.mp4
 python convert.py /gopro /sharphigh 815.mp4
-python convert.py /gopro /sharphigh 604.mp4
-python convert.py /ev0 /sharphigh tornantini.mp4
+#NUKED python convert.py /gopro /sharphigh 604.mp4
+#NUKED python convert.py /ev0 /sharphigh tornantini.mp4
 python convert.py /sharphigh farwest.mp4
 python convert.py /4k /panning forcella_alta_panorama_2.mp4
 python convert.py 812.mp4
 python convert.py /4k /shaky /panning trefaggi_panorama.mp4
 python convert.py trefaggi.mp4
 python convert.py /dark strada_per_blello_selino.mp4
-python convert.py gussa_sud.mp4
-python convert.py tornantini_passaggi.mp4
+#NUKED python convert.py gussa_sud.mp4
+#NUKED python convert.py tornantini_passaggi.mp4
 python convert.py filoncino_passaggi.mp4
-python convert.py gussa_nord_parziale.mp4
+#NUKED python convert.py gussa_nord_parziale.mp4
 python convert.py gussa_ovest.mp4
-python convert.py allegrezza_sud.mp4
+#NUKED python convert.py allegrezza_sud.mp4
 python convert.py linzone_piacca.mp4
 python convert.py piacca_baita_palazzago.mp4
 python convert.py /4k linzone_panorama.mp4
@@ -30,4 +30,15 @@ python convert.py coldara_pedrino_opreno.mp4
 python convert.py opreno_burligo.mp4
 python convert.py filone_easy.mp4
 python convert.py flying_dutchman.mp4
+#NUKED python convert.py /flip 604_busto_bad.mp4
+python convert.py /flip 817_neve_busto.mp4
+python convert.py /flip tornantini_busto.mp4
+python convert.py /flip /dark 604_busto.mp4
+python convert.py /flip sanrocco.mp4
+python convert.py /flip rocce.mp4
+python convert.py /flip scalette.mp4
+python convert.py /flip gussa_est_busto.mp4
+python convert.py /flip gussa_nord_busto.mp4
+python convert.py /flip gussa_sud_busto.mp4
+python convert.py /flip allegrezza_sud_busto.mp4
 
