94bfb7de77783771f307f11de9cde10258a816fa
Andrea Mazzoleni <amadvance@gmail.com>
2019-02-28 12:59:21 +0100

    Don't export metadata
    The GPX info is too unstable.

--- video/export.py
+++ video/export.py
@@ -27,40 +27,10 @@ cmd = open(name + ".sh", "w+")
 lst = open(name + ".lst", "w+")
 
 for a in clips:
-	tmp_file = "tmp" + str(tmp_count) + ".ts"
-	print >>cmd, "ffmpeg -y",
-	print >>cmd, "-ss", float(a["start"]), "-t", float(a["end"]) - float(a["start"]), "-i", a["reader"]["path"],
-	# Use hevc_mp4toannexb instead of h264_mp4toannexb for HVEC
-	# The following is used to copy also the metadata
-	# -map 0:0 -map 0:1 -map 0:2 -map 0:3
-	print >>cmd, "-c copy -map 0:0 -map 0:1 -map 0:2 -map 0:3 -bsf:v h264_mp4toannexb -f mpegts " + tmp_file
-	print >>lst, "file " + tmp_file
-	# The "duration" is requested to avoid frozen frames, caused by a wrong estimation of the segment length
-	# https://stackoverflow.com/questions/32716643/strange-pause-in-video-after-concatenation-of-two-mp4-videos
-	# There is also "inpoint" and "outpoint" but they don't fix the frozen frames issue
-	# https://ffmpeg.org/ffmpeg-formats.html#concat
-	# Using the "concat demuxer" also work without the "duration" needs, but it also doesn't support metadata
-	print >>lst, "duration", float(a["end"]) - float(a["start"])
-	tmp_glob += " " + tmp_file
-	tmp_count = tmp_count + 1
+	print >>lst, "file " + a["reader"]["path"]
+	print >>lst, "inpoint", float(a["start"])
+	print >>lst, "outpoint", float(a["end"])
 
-# Concatenate the video without metadata
-# Metadata can be added with "-map 0:0 -map 0:1 -map 0:3" but then you cannot use "duration" and you get frozen frames
+# Concatenate the video (without metadata)
 print >>cmd, "ffmpeg -y -f concat -i " + name + ".lst -c copy -bsf:a aac_adtstoasc", output_mp4
-
-# Concatenate the metadata
-# With metadata you cannot use "duration", otherwise you get errors like:
-# "Application provided invalid, non monotonically increasing dts to muxer in stream 0: 31303272 >= 31273932"
-print >>cmd, "grep -v duration " + name + ".lst > " + name + ".meta"
-print >>cmd, "ffmpeg -y -f concat -i " + name + ".meta -c copy -map 0:3 -f rawvideo", output_bin
-tmp_glob += " " + name + ".meta"
-
-# Convert binary to gpx
-print >>cmd, "../../go/bin/gopro2gpx -i=" + output_bin + " -o=" + output_gpx
-
-# Convert binary to json
-print >>cmd, "../../go/bin/gopro2json -i=" + output_bin + " -o=" + output_json
-
-# Remove temp files
-print >>cmd, "rm" + tmp_glob
 
