181d960a812047250e86d740c7b43e70439af9d0
Andrea Mazzoleni <amadvance@gmail.com>
2020-10-13 07:14:51 +0200

    New tracks

--- video/all.sh
+++ video/all.sh
@@ -3,31 +3,31 @@ python convert.py /gopro /sharphigh /4k /panning forcella_alta_panorama.mp4
 #NUKED python convert.py /gopro /sharphigh 712sud.mp4
 #NUKED python convert.py /gopro /sharphigh torre.mp4
 #NUKED python convert.py /gopro /sharphigh allegrezza.mp4
-python convert.py /gopro /sharphigh 229.mp4
+#NUKED python convert.py /gopro /sharphigh 229.mp4
 #NUKED python convert.py /gopro /sharphigh pistadh.mp4
-python convert.py /gopro /sharphigh collesogno.mp4
-python convert.py /gopro /sharphigh tesoro.mp4
-python convert.py /gopro /sharphigh 815.mp4
+#NUKED python convert.py /gopro /sharphigh collesogno.mp4
+#NUKED python convert.py /gopro /sharphigh tesoro.mp4
+#NUKED python convert.py /gopro /sharphigh 815.mp4
 #NUKED python convert.py /gopro /sharphigh 604.mp4
 #NUKED python convert.py /ev0 /sharphigh tornantini.mp4
-python convert.py /sharphigh farwest.mp4
+#NUKED python convert.py /sharphigh farwest.mp4
 python convert.py /4k /panning forcella_alta_panorama_2.mp4
 python convert.py 812.mp4
 python convert.py /4k /shaky /panning trefaggi_panorama.mp4
-python convert.py trefaggi.mp4
-python convert.py /dark strada_per_blello_selino.mp4
+#NUKED python convert.py trefaggi.mp4
+#NUKED python convert.py /dark strada_per_blello_selino.mp4
 #NUKED python convert.py gussa_sud.mp4
 #NUKED python convert.py tornantini_passaggi.mp4
 #NUKED python convert.py filoncino_passaggi.mp4
 #NUKED python convert.py gussa_nord_parziale.mp4
 #NUKED python convert.py gussa_ovest.mp4
 #NUKED python convert.py allegrezza_sud.mp4
-python convert.py linzone_piacca.mp4
-python convert.py piacca_baita_palazzago.mp4
+#NUKED python convert.py linzone_piacca.mp4
+#NUKED python convert.py piacca_baita_palazzago.mp4
 python convert.py /4k linzone_panorama.mp4
-python convert.py valcava_funivia_coldara.mp4
-python convert.py coldara_pedrino_opreno.mp4
-python convert.py opreno_burligo.mp4
+#NUKED python convert.py valcava_funivia_coldara.mp4
+#NUKED python convert.py coldara_pedrino_opreno.mp4
+#NUKED python convert.py opreno_burligo.mp4
 #NUKED python convert.py filone_easy.mp4
 python convert.py flying_dutchman.mp4
 #NUKED python convert.py /flip 604_busto_bad.mp4
