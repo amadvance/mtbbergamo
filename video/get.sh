0d79c9b836880c963e7e5afa4089d24ad64e520a
Andrea Mazzoleni <amadvance@gmail.com>
2021-08-16 21:52:28 +0200

    Update get script

--- video/get.sh
+++ video/get.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mtp-filetree | grep -v "DCIM\|100GOPRO\|Get_started" | awk '/^[[:digit:]]/{ print "sleep 5\nmtp-getfile " $1 " " $1 "." gensub(".*\\.","","g",tolower($2)) }' > download.sh
+mtp-filetree | grep -v "DCIM\|100GOPRO\|Get_started" | awk '/^[[:digit:]]/{ print "sleep 5\nmtp-getfile " $1 " " sprintf("%03u",$1) "." gensub(".*\\.","","g",tolower($2)) }' > download.sh
 
 
 
