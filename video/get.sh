8f8635a0b4b38d4208f584a62fd9b17ac2353ef3
Andrea Mazzoleni <amadvance@gmail.com>
2022-02-28 16:54:05 +0100

    Update download script for gopro

--- video/get.sh
+++ video/get.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-mtp-filetree | grep -v "DCIM\|100GOPRO\|Get_started" | awk '/^[[:digit:]]/{ print "sleep 5\nmtp-getfile " $1 " " sprintf("%03u",$1) "." gensub(".*\\.","","g",tolower($2)) }' > download.sh
+echo set -e > download.sh
+mtp-filetree | grep -v "DCIM\|100GOPRO\|Get_started" | awk '/^[[:digit:]]/{ print "sleep 5\nmtp-getfile " $1 " " sprintf("%03u",$1) "." gensub(".*\\.","","g",tolower($2)) }' >> download.sh
+echo echo Everything OK >> download.sh
 
 
 
