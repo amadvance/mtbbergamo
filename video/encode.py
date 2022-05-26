ebb1fb53e2cb54207788c378d2cab69e33178efc
Andrea Mazzoleni <amadvance@gmail.com>
2022-05-26 19:11:24 +0200

    Use the concat filter instead of the concat demuxer to have more precise cutting

--- video/encode.py
+++ video/encode.py
@@ -17,8 +17,6 @@ with open(file) as f:
 name = os.path.splitext(os.path.basename(file))[0]
 output_mp4 = name + "-YOUTUBE.mp4"
 output_lst = name + ".lst";
-tmp_glob = ""
-tmp_count = 0
 ffmpeg = "../../ffmpeg-5.0.1-amd64-static/ffmpeg"
 
 # Sort the clips, they are in insertion order, not in position order
@@ -32,32 +30,21 @@ files = set()
 print >>cmd, "set -e"
 
 # Generate the list file for the concat
-print >>cmd, "rm -f " + output_lst
+input_list = ""
+filter_list = ""
+i = 0
 for a in clips:
 	clip_mp4 = a["reader"]["path"].replace("./","")
-	clip_mkv = clip_mp4.replace(".mp4",".tmp.mkv")
-	print >>cmd, "echo file " + clip_mkv, " >> " + output_lst
-	print >>cmd, "echo inpoint", float(a["start"]), ">> " + output_lst
-	print >>cmd, "echo outpoint", float(a["end"]), ">> " + output_lst
-	files.add(clip_mp4)
+	input_list += "-ss " + str(float(a["start"])) + " -to " + str(float(a["end"])) + " -i " + clip_mp4 + " "
+	filter_list += "[" + str(i) + ":v] [" + str(i) + ":a] "
+	i += 1
 
-# Repack the clips to mkv
-# Using the originals result in the contact to insert frozen frames at the startup
-# Note that repacking in mp4 still has this problem
-for a in files:
-	clip_mp4 = a
-	clip_mkv = clip_mp4.replace(".mp4",".tmp.mkv")
-	tmp_glob += " " + clip_mkv
-	print >>cmd, ffmpeg, "-y -i", clip_mp4, "-c copy -map 0:0 -map 0:1", clip_mkv
+filter_list += "concat=n=" + str(i) + ":v=1:a=1 [v] [a]"
 
 # Concatenate the video with encoding
 PLAIN='-c copy -bsf:a aac_adtstoasc'
-FILTER='-vf "normalize=blackpt=black:whitept=white:smoothing=60:strength=1.0,eq=contrast=1.1:saturation=1.5,curves=blue=\'0/0 0.5/0.45 1/1\',unsharp=7:7:1.2,hflip,vflip"'
-FILTER_DARK='-vf "curves=all=\'0/0 0.5/0.58 1/1\',normalize=blackpt=black:whitept=white:smoothing=60:strength=1.0,eq=saturation=1.5,unsharp=7:7:1.2,hflip,vflip"'
+FILTER='normalize=blackpt=black:whitept=white:smoothing=60:strength=1.0,eq=contrast=1.1:saturation=1.5,curves=blue=\'0/0 0.5/0.45 1/1\',unsharp=7:7:1.2'
+FILTER_DARK='curves=all=\'0/0 0.5/0.58 1/1\',normalize=blackpt=black:whitept=white:smoothing=60:strength=1.0,eq=saturation=1.5,unsharp=7:7:1.2'
 ENCODE='-preset veryfast -codec:v libx264 -crf 24 -maxrate 90M -bufsize 30M -pix_fmt yuvj420p -codec:a aac -b:a 192k'
-print >>cmd, ffmpeg, "-y -f concat -i " + name + ".lst", FILTER, ENCODE, output_mp4
-
-# Remove temp files
-print >>cmd, "rm" + tmp_glob
-print >>cmd, "rm " + output_lst
+print >>cmd, ffmpeg, "-y", input_list, "-filter_complex \"" + filter_list + ";[v]" + FILTER + "[v]\" -map \"[v]\" -map \"[a]\"", ENCODE, output_mp4
 
