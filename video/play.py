8e05ae3e69c3f914e0bd5050c1c01f4fce2d6c0c
Andrea Mazzoleni <amadvance@gmail.com>
2022-08-22 18:14:09 +0200

    New play scripts

--- video/play.py
+++ video/play.py
@@ -9,10 +9,9 @@ if len(sys.argv) != 2:
 
 file = sys.argv[1]
 
-FILTER='-vf "scale=-1:720,normalize=blackpt=black:whitept=white:smoothing=60:strength=1.0,eq=contrast=1.1:saturation=1.5,curves=blue=\'0/0 0.5/0.45 1/1\',unsharp=7:7:1.2"'
-FILTER_DARK='-vf "scale=-1:720,curves=all=\'0/0 0.4/0.47 1/1\',normalize=blackpt=black:whitept=white:smoothing=60:strength=1.0,eq=saturation=1.5,unsharp=7:7:1.2"'
+FILTER='normalize=blackpt=black:whitept=white:smoothing=60:strength=1.0,eq=contrast=1.1:saturation=1.5,curves=blue=\'0/0 0.5/0.45 1/1\',unsharp=7:7:1.2'
 
-cmdline = "ffplay -i " + file + " " + FILTER_DARK
+cmdline = "ffplay -i " + file + " -vf \"scale=-1:720," + FILTER + "\""
 
 # Run
 print '$', cmdline
