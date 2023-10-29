d585df63afd512d033577c7a0dc2c2f778df3bd3
Andrea Mazzoleni <amadvance@gmail.com>
2023-10-29 15:32:51 +0100

    Rename scripts

--- video/get.sh
+++ video/get.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo set -e > download.sh
-mtp-filetree | grep -E "\.MP4|\.JPG" | awk -f ../get.awk >> download.sh
-echo echo Everything OK >> download.sh
+echo set -e > GET.sh
+mtp-filetree | grep -E "\.MP4|\.JPG" | awk -f ../get.awk >> GET.sh
+echo echo Everything OK >> GET.sh
