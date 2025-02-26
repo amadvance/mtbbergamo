596a651b210f3916ff795764ba4007f2aabb0fd1
Andrea Mazzoleni <amadvance@gmail.com>
2025-02-26 16:44:54 +0100

    New tracks

--- www/all.js
+++ www/all.js
@@ -1990,7 +1990,7 @@ removed su richiesta del CAI Albino
 { vote: 5, rate: 2, rate_max: 3, cycle: 0, zone: 'bassa_seriana', name: 'Monte Altino, Dossello, Cene', kind: 'down', file: 'Seriana_MonteAltinoDosselloCene.gpx', link: '2024/12/altino-dossello' },
 { vote: 4, rate: 3, rate_max: 0, cycle: 0, zone: 'bassa_seriana', name: 'Monte Altino, Grumelduro, Cene', kind: 'down', file: 'Seriana_MonteAltinoGrumelduro.gpx', link: '2025/01/altino-grumelduro' },
 { vote: 4, rate: 3, rate_max: 0, cycle: 0, zone: 'bassa_seriana', name: 'Monte Altino, Abbazia', kind: 'down', file: 'Seriana_MonteAltinoAbbazia.gpx', link: '2025/01/altino-abbazia' },
-{ vote: 5, rate: 1, rate_max: 0, cycle: 0, zone: 'bassa_seriana', name: 'Monte Beio DH, Cene', kind: 'down', file: 'Seriana_MonteBeioDHCene.gpx' },
+{ vote: 5, rate: 1, rate_max: 0, cycle: 0, zone: 'bassa_seriana', name: 'Monte Beio DH, Cene', kind: 'down', file: 'Seriana_MonteBeioDHCene.gpx', link: '2025/02/beio-dh' },
 
 // climb
 { vote: 0, rate: 0, rate_max: 0, cycle: 0, zone: 'bassa_seriana, misma', name: 'Salita da Pradalunga, Morosini, Ripetitori, Monte Misma', kind: 'up', file: 'Seriana_SalitaMismaRipetitori_EBIKE.gpx', link: '2021/04/misma-santa-maria-cenate-sopra' },
@@ -2009,7 +2009,7 @@ Con una MTB conviene fare la salita alternativa, che passa da Fiobbio, e prosegu
 { vote: 0, rate: 0, rate_max: 0, cycle: 0, zone: 'bassa_seriana', name: 'Salita Cene, Monte Altino', kind: 'up', file: 'Seriana_SalitaCeneMonteAltino_EBIKE.gpx', link: '2024/11/altino' },
 { vote: 0, rate: 0, rate_max: 0, cycle: 0, zone: 'bassa_seriana', name: 'Salita Cene, Santuario di Altino, Periplo e cima Monte Altino', kind: 'up', file: 'Seriana_SalitaCenePeriploCimaMonteAltino_EBIKE.gpx', link: '2024/12/altino-dossello' },
 { vote: 0, rate: 0, rate_max: 0, cycle: 1, zone: 'bassa_seriana', name: 'Salita Abbazia, Prati Moletti, Santuario di Altino, Monte Altino', kind: 'up', file: 'Seriana_AbbaziaMolettiMonteAltino_EBIKE.gpx', link: '2025/01/altino-abbazia' },
-{ vote: 0, rate: 0, rate_max: 0, cycle: 1, zone: 'bassa_seriana', name: 'Salita Cene, Monte Beio', kind: 'up', file: 'Seriana_SalitaCeneMonteBeio_EBIKE.gpx' },
+{ vote: 0, rate: 0, rate_max: 0, cycle: 1, zone: 'bassa_seriana', name: 'Salita Cene, Monte Beio', kind: 'up', file: 'Seriana_SalitaCeneMonteBeio_EBIKE.gpx', link: '2025/02/beio-dh' },
 
 // overlapping
 { vote: 0, rate: 0, rate_max: 0, cycle: 0, zone: '', name: 'Salita da Ganda al Monte Poieto', kind: 'up', file: 'Seriana_SalitaGandaPoieto.gpx', link: '2019/03/salite-valle-seriana' },
